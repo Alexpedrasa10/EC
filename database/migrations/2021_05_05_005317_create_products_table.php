6fc4629786b66c1b757f2ee43ed24224436be2c4
Alexpedrasa10 <Alexpedrasa10@gmail.com>
2021-09-15 21:31:43 -0300

    fix

--- database/migrations/2021_05_05_005317_create_products_table.php
+++ database/migrations/2021_05_05_005317_create_products_table.php
@@ -24,10 +24,12 @@ class CreateProductsTable extends Migration
             $table->float('sale_price')->nullable();
             $table->string('code')->nullable();
             $table->jsonb('data')->nullable();
+            $table->integer('photo_id');
             $table->boolean('is_active')->default(true);
             $table->timestamps();
 
             $table->index(array('slug', 'code'), 'product_index');
+            $table->index(array('photo_id'), 'photo_product_index');
         });
     }
 
